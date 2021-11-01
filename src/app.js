e560fb24f88d8106234f749be1b9d15616bd6d8d
wkrn <divinorum21rt@gmail.com>
2021-11-01 10:56:45 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boilerplate.</p>, document.getElementById('app'))
+
+
