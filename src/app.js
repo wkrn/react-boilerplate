61cf83453da3a5bcd9acc650015e418ade9c6940
wkrn <divinorum21rt@gmail.com>
2021-11-01 11:07:43 +0900

    historyApiFallback=True

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate.</p>, document.getElementById('app'))
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboards
+    </div>
+)
+const AddExpensePage = () => (
+    <div>
+        Add Expense Page
+    </div>
+)
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage} />
+        </div>
+    </BrowserRouter>
+)
+ReactDOM.render(routes, document.getElementById('app'))
 
 
