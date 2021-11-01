a9e4abb6c5f15c4645a8d638baee40cfb7f1b38c
wkrn <divinorum21rt@gmail.com>
2021-11-01 11:35:48 +0900

    Divided Into Components

--- src/app.js
+++ src/app.js
@@ -1,46 +1,11 @@
 
-import ReactDOM from 'react-dom';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboards
-    </div>
-)
-const AddExpensePage = () => (
-    <div>
-        Add Expense Page
-    </div>
-)
-const EditExpensePage = () => (
-    <div>
-        Edit Expense Page
-    </div>
-)
-const HelpPage = () => (
-    <div>
-        Help Page
-    </div>
-)
-const NotFoundPage = () => (
-    <div>
-        404 Not Found
-    </div>
-)
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </BrowserRouter>
-)
-ReactDOM.render(routes, document.getElementById('app'))
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
 
 
---
+++ src/components/HelpPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const HelpPage = () => (
+    <div>
+        Help Page
+    </div>
+)
+
+export default HelpPage
