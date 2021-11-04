f82e9506b15214bf5f869a5707027308c52ab7c3
wkrn <divinorum21rt@gmail.com>
2021-11-04 13:52:26 +0900

    Added Redux

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,6 +15,7 @@ const AppRouter = () => (
                 <Route path="/" component={ExpenseDashboardPage} exact={true} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
