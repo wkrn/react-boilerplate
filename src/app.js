227d4310a594c4573fa4954d4698458dd9ac4832
wkrn <divinorum21rt@gmail.com>
2021-12-07 18:53:24 +0900

    app.jsのdispatchを咲くs所

--- src/app.js
+++ src/app.js
@@ -3,25 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 7000, createdAt: 1638868659013 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 6600, createdAt: 1638868659013 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 60000, createdAt: 1638868659013 }));
-// store.dispatch(setTextFilter('water'));
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses)
-// console.log(state)
 const jsx = (
     <Provider store={store}>
         <AppRouter />
