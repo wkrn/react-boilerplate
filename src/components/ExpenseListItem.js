2710d8203927ab5d9b92ef75b6a5f8920d034979
wkrn <divinorum21rt@gmail.com>
2021-12-07 22:40:34 +0900

    numeralを導入してamountの表示を$にした

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,17 @@
 import moment from 'moment';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {moment(createdAt).format("YYYY-MM-DD")}</p>
+        <p>
+            {numeral(amount / 100).format('$0.0.00')}
+            -
+            {moment(createdAt).format("YYYY-MM-DD")}</p>
 
     </div >
 )
