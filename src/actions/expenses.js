8cf6d702a46d699f5e0131d46b4353cca44071f9
wkrn <divinorum21rt@gmail.com>
2022-02-11 17:17:46 +0900

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid'
+import ExpensesSummary from '../components/ExpensesSummary';
 import database from '../firebase/firebase';
 
 export const addExpense = (expense) => ({
@@ -37,4 +38,24 @@ export const editExpense = (id, updates) => (
         updates
     }
 )
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
 
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
+import { setTextFilter } from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -14,7 +17,11 @@ const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider >
-)
-ReactDOM.render(jsx, document.getElementById('app'))
+);
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'))
+})
 
 
