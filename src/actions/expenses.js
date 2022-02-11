b26aa5172f1d900e9ae05af15dae17ba8c8c28c6
wkrn <divinorum21rt@gmail.com>
2022-02-11 17:32:47 +0900

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -31,6 +31,14 @@ export const removeExpense = ({ id } = {}) => ({
     id
 })
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }))
+        })
+    }
+}
+
 export const editExpense = (id, updates) => (
     {
         type: 'EDIT_EXPENSE',
@@ -58,4 +66,5 @@ export const startSetExpenses = () => {
         });
     };
 };
+
 
