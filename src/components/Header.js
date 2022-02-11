7110b56ab00ce8f2c0dc78cc2d58be14514b848c
wkrn <divinorum21rt@gmail.com>
2022-02-11 19:44:07 +0900

    Added private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-const Header = () => (
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
+
+export const Header = ({ startLogout }) => (
     <header>
         <h2>Expensify</h2>
         <NavLink to="/" activeClassName="is-active" exact={true}>DashBoard </NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create </NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expenses</NavLink>
+        <button onClick={startLogout}>Logout</button>
     </header>
 )
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
